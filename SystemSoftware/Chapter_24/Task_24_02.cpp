71018c53ae6bc429888c5a201c2bb8fb7556358a
Damir Garifullin <gosugdr@gmail.com>
2017-05-03 19:24:57 +0300

    Done chapter 24

--- SystemSoftware/Chapter_24/Task_24_02.cpp
+++ SystemSoftware/Chapter_24/Task_24_02.cpp
@@ -3,9 +3,26 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-
+    ifstream in;
+    in.open("Task_24_02.dat", ios::binary);
+    vector<int> vec;
+    int temp;
+    while (in >> temp) {
+        vec.push_back(temp);
+    }
+    in.close();
+    ofstream out;
+    out.open("Task_24_02.dat", ios::binary | ios::app);
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i] % 2 == 0) {
+            out << vec[i];
+        }
+    }
+    out.close();
 }
--- SystemSoftware/Chapter_24/Task_24_08.cpp
+++ SystemSoftware/Chapter_24/Task_24_08.cpp
@@ -2,3 +2,34 @@
 // Created by Damir Garifullin on 25/04/2017.
 //
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ifstream in;
+    ofstream out;
+    in.open("Task_24_08.dat", ios::binary);
+    vector<int> vec;
+    int temp;
+    while(in >> temp) {
+        vec.push_back(temp);
+    }
+    in.close();
+    out.open("Task_24_04.dat", ios::binary | ios::app);
+
+    out.seekp(0, ios::end);
+    long pos = out.tellp() / 2;
+    out.seekp(pos);
+
+    int value;
+    cin >> value;
+    for (int i = 0; i < vec.size(); i++) {
+        if (value > vec[i]) {
+            out << vec[i];
+        }
+    }
+    return 0;
+}
