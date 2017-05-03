47d818f1d56a0ee6b892a52004723d2b0d7182ff
Damir Garifullin <gosugdr@gmail.com>
2017-05-03 19:33:44 +0300

    Done chapter 3

--- SystemSoftware/Chapter_03/Task_3_02.cpp
+++ SystemSoftware/Chapter_03/Task_3_02.cpp
@@ -2,3 +2,24 @@
 // Created by Damir Garifullin on 25/04/2017.
 //
 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double pow(double a) {
+    return a * a;
+}
+
+int main() {
+    double a, x;
+    cin >> a >> x;
+
+    if (a < 0) {
+        cout << a + pow(tan(x*x*x));
+    } else {
+        cout << sqrt(a + x*x*x*x);
+    }
+
+    return 0;
+}
--- SystemSoftware/Chapter_03/Task_3_04.cpp
+++ SystemSoftware/Chapter_03/Task_3_04.cpp
@@ -2,3 +2,21 @@
 // Created by Damir Garifullin on 25/04/2017.
 //
 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double pow(double a) {
+    return a * a;
+}
+
+int main() {
+    double a, x;
+    cin >> a >> x;
+    if (sin(a) > x) {
+        cout << (x + 9 - pow(sin(x))) / (cbrt(sin(3 * a)));
+    } else {
+        cout << (exp(sin(a * x)))/(pow(cos(a * x)));
+    }
+}
