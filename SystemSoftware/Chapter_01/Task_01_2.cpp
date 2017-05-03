260b6394935f73d6320da1c2267b789bdd9f8fc2
Damir Garifullin <gosugdr@gmail.com>
2017-05-03 19:37:47 +0300

    Done chapter 1

---
+++ SystemSoftware/Chapter_01/Task_01_2.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Damir Garifullin on 03/05/2017.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float r;
+    cout << "Input value of a ";
+    cin >> a; // Не инициализированно
+    cout << "Input value of b ";
+    cin >> b; // Не инициализированно
+    r = a * b;
+    cout << "r = " << r << endl;
+    return 0;
+}
---
+++ SystemSoftware/Chapter_01/Task_01_4.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Damir Garifullin on 03/05/2017.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int p, l, m;
+
+    cout << "Input value of m ";
+    cin >> m;
+    cout << "Input value of l";
+    cin >> l;
+    l = 2; m = 9;
+    p = m + l;
+    cout << "p = " << p << endl;
+    return 0;
+}
+
+// Вывод: 11
